87dc4a9cfac226d3f9ca796604b77f62e2f6efbd
nicoargiolas <argiolas.nicola00@gmail.com>
2024-12-18 12:42:51 +0100

    Descrizione passaggi

---
+++ script.js
@@ -0,0 +1,27 @@
+// Esercizio di oggi: FizzBuzz
+// Consegna:
+// Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Come faccio a sapere se un numero è divisibile per?
+// Abbiamo visto qualcosa di particolare che possiamo usare?
+// Consigli del giorno:
+// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// 2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo “a mano”;
+// Dopo esserci concentrati sul far funzionare le cose e averle testate,
+// ragioniamo  sul possibile refactoring, quindi ottimizzazione possibile,
+// sia del codice come performance, ma anche, se non soprattutto come leggibilità e mantenibilità.
+
+// Inizio del ciclo
+
+// Riconoscimento dei numeri multipli di 3 o di 5
+
+    // Riconoscimento dei numeri multipli di 3
+    // In questo caso stamperemo Fizz
+
+    // Riconoscimento dei numeri multipli di 5
+    // In questo caso stamperemo Buzz
+
+    // Se i numeri saranno multipli sia di 3 che di 5 stamperemo FizzBuzz
+
+// Altrimenti stamperemo il numero
